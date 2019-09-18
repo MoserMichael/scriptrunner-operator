a5f5b546f61dbf14c63860e4b9e76c167158c6e9
MichaelMoser <mmoser@localhost.localdomain>
2019-09-18 22:34:59 +0300

    operator-sdk: generate initial CRD definition (empty)
        export GOROOT=$(dirname $(which go))
        operator-sdk add api --api-version=scriptrunner.github.com/v1alpha1 --kind=ScriptRunner

---
+++ pkg/apis/scriptrunner/group.go
@@ -0,0 +1,6 @@
+// Package scriptrunner contains scriptrunner API versions.
+//
+// This file ensures Go source parsers acknowledge the scriptrunner package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package scriptrunner
